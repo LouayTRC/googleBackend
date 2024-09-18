f8f9c9d48727b9cd4973353bcc955f9f9de735b4
louuu <louaytrc@gmail.com>
2024-09-18 17:50:31 +0100

    deployed git add .

--- src/main/java/club_website/auth/AuthApplication.java
+++ src/main/java/club_website/auth/AuthApplication.java
@@ -9,9 +9,7 @@ public class AuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
-		System.out.println("works");
-		System.out.println("DB URL: " + System.getenv("DB_URL"));
-	    System.out.println("DB Username: " + System.getenv("DB_USERNAME"));
+		System.out.println("works");	
 	}
 
 }
